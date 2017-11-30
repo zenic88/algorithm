6b3ace8a881d22b45b0a471b7f44fb93141b32fa
Jeong Eunwoo <zenic88@naver.com>
2017-11-30 21:16:08 +0900

    1학년 문제

---
+++ firstGrade/src/com/mscbrain/Main.java
@@ -0,0 +1,67 @@
+package com.mscbrain;
+
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    String numCnt = sc.nextLine();
+    String str = sc.nextLine();
+
+//    System.out.println(numCnt);
+//    System.out.println(str);
+
+    long l = System.currentTimeMillis();
+
+    String[] strArr = str.split(" ");
+
+//    System.out.println(Arrays.toString(list.toArray()));
+    int firstNum = Integer.parseInt(strArr[0]);
+
+    int count = calculate(firstNum, strArr, 1, Integer.parseInt(numCnt) - 1);
+
+    long interval = System.currentTimeMillis() - l;
+    if(interval < 1000){
+      System.out.println("0." + interval + "초");
+    }else{
+      System.out.println(interval / 1000 + "초");
+    }
+
+//    System.out.println("dp 종료");
+    System.out.println(count);
+
+  }
+
+  public static int calculate(int firstNum, String[] strArr, int i, int testCase){
+    int count = 0;
+
+    if(firstNum < 0 || firstNum > 20){
+      return 0;
+    }
+
+    if(i < testCase){
+      int secondNum = Integer.parseInt(strArr[i]);
+
+//      System.out.println("firstNum : " + firstNum + " secondNum : " + secondNum + " " + i);
+
+      i++;
+
+      int sum = firstNum + secondNum;
+      int gap = firstNum - secondNum;
+
+//      System.out.println("합 : " + sum + " 차 : " + gap);
+
+      count += calculate(sum, strArr, i, testCase);
+      count += calculate(gap, strArr, i, testCase);
+    }else{
+//      System.out.println("계산 결과 : " + firstNum + " 제일 뒤에 수 : " + list.get(i));
+      if(firstNum == Integer.parseInt(strArr[i])){
+        count++;
+      }
+    }
+
+    return count;
+  }
+}
